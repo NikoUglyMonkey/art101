8ccdd50cbcf81e25fa9e4b5c329c38de0989c02f
NikoUglyMonkey <nikonissen@yahoo.com>
2024-05-12 13:14:15 -0700

    work

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,22 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+index.js - Lab 9 - Libraries & jQuery - This jQuery script uses buttons to modify elements on the page.
+Requirements: jQuery must be loaded for this script to work.
+Author: Niko Nissen
+Date: 5/12/2024
+*/
 
-// Constants
+ // add button to the challenge section
+  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
-
+// add a click listener to the challenge button
+  $("button-challenge").click(function(){
+//now add or subtract the special class to the section
+  $("#challenge").toggleClass("special");
+});
