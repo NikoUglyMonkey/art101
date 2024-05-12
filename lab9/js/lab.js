124b4d6508adc56c77e95c66c68b94c7f503b41a
NikoUglyMonkey <nikonissen@yahoo.com>
2024-05-12 14:51:13 -0700

    lab 9 completed

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,11 +4,26 @@ Author: Niko Nissen
 Date: 5/12/2024
 */
 
-//add a button to the challenge section
-  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-
+  //add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Click Here!</button>");
 //add a click listener to the challenge button
-  $("#challenge").click(function(){
-  //add or subtract the special class to the section
-  $("#challenge").toggleClass("special");
+$("#button-challenge").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
+
+//add button to problems section
+$("#problems").append("<button id='button-problems'>Click Here!</button>");
+//add a click listener to the problems button
+$("#button-problems").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+
+//add button to results section
+$("#results").append("<button id='button-results'>Click here!</button>");
+//add a click listener to the results button
+$("#button-results").click(function(){
+    //now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
 });
